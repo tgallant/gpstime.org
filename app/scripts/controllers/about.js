d5a8b0ffd43b93ec5beb49ce6e8fd6cf9486271b
Tim Gallant <tim.gallant@gomural.com>
2014-12-05 11:54:41 -0700

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gpstimeorgApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gpstimeorgApp
+ */
+angular.module('gpstimeorgApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
